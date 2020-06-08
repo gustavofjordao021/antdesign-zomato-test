a3124015f754366e89eb9c0d97101eb303862739
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-06-08 09:15:24 -0400

    React Hooks + Ant Design practice

---
+++ zomato-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import HomeLayout from "./HomeLayout/homeLayout";
+
+function App() {
+  return (
+    <div className="App" style={{ height: "100%" }}>
+      <HomeLayout />
+    </div>
+  );
+}
+
+export default App;
