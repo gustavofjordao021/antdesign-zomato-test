a93543f3d09e02441dd434b253865fe661a7d361
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-06-16 16:16:31 -0400

    Setup Material-UI dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import HomeLayout from "./HomeLayout/homeLayout.js";
+// import LandingPage from "./views/LandingPage/LandingPage.js";
 
 function App() {
   return (
+    // <LandingPage />
     <div className="App" style={{ height: "100%" }}>
       <a href="http://localhost:3001/auth/facebook">Login with Facebook</a>
     </div>
