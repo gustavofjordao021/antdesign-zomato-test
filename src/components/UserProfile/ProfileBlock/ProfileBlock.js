ed514cc596f48148e75469080997b4ea015e3970
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-08-09 18:31:36 -0400

    User profile work

---
+++ src/components/UserProfile/ProfileBlock/ProfileBlock.js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+
+import "./ProfileBlock.css";
+
+import { Row, Col, Card, Form, Input, CardBody, FormGroup } from "reactstrap";
+
+const ProfileBlock = (props) => {
+  const {
+    userLoggedIn: { email, username, firstName, lastName },
+  } = props;
+  return (
+    <Card className="m-5">
+      <CardBody>
+        <Form>
+          <h6 className="heading-small text-muted mb-4">User information</h6>
+          <div className="pl-lg-4">
+            <Row>
+              <Col lg="6">
+                <FormGroup>
+                  <label
+                    className="form-control-label"
+                    htmlFor="input-username"
+                  >
+                    Username
+                  </label>
+                  <Input
+                    className="form-control-alternative"
+                    defaultValue={username}
+                    id="input-username"
+                    placeholder="Username"
+                    type="text"
+                  />
+                </FormGroup>
+              </Col>
+              <Col lg="6">
+                <FormGroup>
+                  <label className="form-control-label" htmlFor="input-email">
+                    Email address
+                  </label>
+                  <Input
+                    className="form-control-alternative"
+                    id="input-email"
+                    defaultValue={email}
+                    placeholder="Email"
+                    type="email"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="6">
+                <FormGroup>
+                  <label
+                    className="form-control-label"
+                    htmlFor="input-first-name"
+                  >
+                    First name
+                  </label>
+                  <Input
+                    className="form-control-alternative"
+                    defaultValue={firstName}
+                    id="input-first-name"
+                    placeholder="First name"
+                    type="text"
+                  />
+                </FormGroup>
+              </Col>
+              <Col lg="6">
+                <FormGroup>
+                  <label
+                    className="form-control-label"
+                    htmlFor="input-last-name"
+                  >
+                    Last name
+                  </label>
+                  <Input
+                    className="form-control-alternative"
+                    defaultValue={lastName}
+                    id="input-last-name"
+                    placeholder="Last name"
+                    type="text"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+          </div>
+          <hr className="my-4" />
+          {/* Description */}
+          <h6 className="heading-small text-muted mb-4">About me</h6>
+          <div className="pl-lg-4">
+            <FormGroup>
+              <label>About Me</label>
+              <Input
+                className="form-control-alternative"
+                placeholder="A few words about you ..."
+                rows="4"
+                defaultValue="A beautiful Dashboard for Bootstrap 4. It is Free and
+      Open Source."
+                type="textarea"
+              />
+            </FormGroup>
+          </div>
+        </Form>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ProfileBlock;
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -3,6 +3,7 @@ import { Redirect } from "react-router-dom";
 import { AuthContext } from "../../context/index";
 
 import MainNav from "../Navbar/MainNav/MainNav";
+import ProfileBlock from "../UserProfile/ProfileBlock/ProfileBlock";
 
 import "./UserProfile.css";
 
@@ -23,7 +24,6 @@ const UserProfile = () => {
     <AuthContext.Consumer>
       {(context) => {
         const { currentUser, isLoggedIn } = context.state;
-        const { username, email } = currentUser;
         return (
           <>
             {!isLoggedIn ? (
@@ -35,10 +35,10 @@ const UserProfile = () => {
                     userLoggedIn={currentUser}
                     passedDownToggleGoalForm={() => this.toggleGoalFormOn()}
                   />
-                  <Col className="p-0 flex-container main-container full-height full-width">
+                  <Col className="p-0 flex-container main-container full-height full-width bg-secondary">
                     <Card className="full-width full-height bg-secondary shadow main-container">
-                      <CardHeader className="bg-white border-0 full-width">
-                        <Row className="align-items-center">
+                      <CardHeader className="bg-white full-width">
+                        <Row className="align-items-center flex-center">
                           <Col xs="8">
                             <h3 className="mb-0">My account</h3>
                           </Col>
@@ -52,107 +52,10 @@ const UserProfile = () => {
                               Settings
                             </Button>
                           </Col>
+                          <ProfileBlock userLoggedIn={currentUser} />
+                          <ProfileBlock userLoggedIn={currentUser} />
                         </Row>
                       </CardHeader>
-                      <Card>
-                        <CardBody>
-                          <Form>
-                            <h6 className="heading-small text-muted mb-4">
-                              User information
-                            </h6>
-                            <div className="pl-lg-4">
-                              <Row>
-                                <Col lg="6">
-                                  <FormGroup>
-                                    <label
-                                      className="form-control-label"
-                                      htmlFor="input-username"
-                                    >
-                                      Username
-                                    </label>
-                                    <Input
-                                      className="form-control-alternative"
-                                      id="input-username"
-                                      placeholder={username}
-                                      type="text"
-                                    />
-                                  </FormGroup>
-                                </Col>
-                                <Col lg="6">
-                                  <FormGroup>
-                                    <label
-                                      className="form-control-label"
-                                      htmlFor="input-email"
-                                    >
-                                      Email address
-                                    </label>
-                                    <Input
-                                      className="form-control-alternative"
-                                      id="input-email"
-                                      placeholder={email}
-                                      type="email"
-                                    />
-                                  </FormGroup>
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col lg="6">
-                                  <FormGroup>
-                                    <label
-                                      className="form-control-label"
-                                      htmlFor="input-first-name"
-                                    >
-                                      First name
-                                    </label>
-                                    <Input
-                                      className="form-control-alternative"
-                                      defaultValue="Lucky"
-                                      id="input-first-name"
-                                      placeholder="First name"
-                                      type="text"
-                                    />
-                                  </FormGroup>
-                                </Col>
-                                <Col lg="6">
-                                  <FormGroup>
-                                    <label
-                                      className="form-control-label"
-                                      htmlFor="input-last-name"
-                                    >
-                                      Last name
-                                    </label>
-                                    <Input
-                                      className="form-control-alternative"
-                                      defaultValue="Jesse"
-                                      id="input-last-name"
-                                      placeholder="Last name"
-                                      type="text"
-                                    />
-                                  </FormGroup>
-                                </Col>
-                              </Row>
-                            </div>
-                            <hr className="my-4" />
-                            {/* Description */}
-                            <h6 className="heading-small text-muted mb-4">
-                              About me
-                            </h6>
-                            <div className="pl-lg-4">
-                              <FormGroup>
-                                <label>About Me</label>
-                                <Input
-                                  className="form-control-alternative"
-                                  placeholder="A few words about you ..."
-                                  rows="4"
-                                  defaultValue="A beautiful Dashboard for Bootstrap 4. It is Free and
-                          Open Source."
-                                  type="textarea"
-                                />
-                              </FormGroup>
-                            </div>
-                          </Form>
-                        </CardBody>
-                      </Card>
                     </Card>
                   </Col>
                 </Row>
