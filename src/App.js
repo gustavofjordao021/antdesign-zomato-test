f914c069658e1a6e63b00a637aea5b1173fd8303
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-08-22 12:14:55 -0400

    Redeploying with fixes to path

--- src/App.js
+++ src/App.js
@@ -27,15 +27,16 @@ const App = () => {
         return (
           <div className="App">
             <Switch>
-              <Route path="/" component={Home} />
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/app" component={ApplicationHome} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/app" component={ApplicationHome} />
               <Route
+                exact
                 path="/app/collections/:collectionId"
                 component={GoalDetails}
               />
-              <Route path="/app/user-profile" component={UserProfile} />
+              <Route exact path="/app/user-profile" component={UserProfile} />
             </Switch>
             {isLoggedIn ? <span /> : <Footer />}
           </div>
